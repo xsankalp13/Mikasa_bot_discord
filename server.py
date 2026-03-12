fdc6e6da91c81a0d565ef16e021b85db836cf925
xSankalp13 <sankalpbsalve@gmail.com>
2026-03-12 19:22:15 +0530

    doc(policies): added term of service and privacy policies.

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from threading import Thread
+import os
 
 app = Flask('')
+
 @app.route('/')
 def home():
     return "I'm alive"
 
+@app.route('/terms-of-service')
+def terms():
+    return send_from_directory('static', 'terms.html')
+
+@app.route('/privacy-policy')
+def privacy():
+    return send_from_directory('static', 'privacy.html')
+
 def run():
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t= Thread(target=run)
+    t = Thread(target=run)
     t.start()
